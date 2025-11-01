789fd1fab2cddadf9a312aa9e5ae8bd035c11020
wanerge <wanerge100@hotmail.com>
2025-11-01 04:38:21 -0500

    Se realizaron los codelabs y el lab2

---
+++ lab2/basic-android-kotlin-compose-training-mars-photos-repo-starter/app/src/test/java/com/example/marsphotos/NetworkMarsRepositoryTest.kt
@@ -0,0 +1,20 @@
+package com.example.marsphotos
+
+import com.example.marsphotos.data.NetworkMarsPhotosRepository
+import com.example.marsphotos.fake.FakeDataSource
+import com.example.marsphotos.fake.FakeMarsApiService
+import org.junit.Test
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+
+class NetworkMarsRepositoryTest {
+
+    @Test
+    fun networkMarsPhotosRepository_getMarsPhotos_verifyPhotoList() =
+        runTest {
+            val repository = NetworkMarsPhotosRepository(
+                marsApiService = FakeMarsApiService()
+            )
+            assertEquals(FakeDataSource.photosList, repository.getMarsPhotos())
+        }
+}
